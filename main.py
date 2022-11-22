712cf62afa6051dfddbac1133c125464ca5ba44f
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2022-11-22 23:38:56 +0100

    update main story 1,2,3

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from opencv import open_img, blur_img
+from filters import black_white, blur, dilation
+import manage
 
-open_img("assets/megumin.jpeg")
-blur_img("assets/megumin.jpeg")
+#manage.open_img("imgs/megumin.jpeg")
+black_white.black_white_img("imgs/megumin.jpeg")
+blur.blur("imgs/megumin.jpeg",55,"GaussianBlur") # GaussianBlur, MedianBlur, Blur
+dilation.dilation("imgs/megumin.jpeg",10)
