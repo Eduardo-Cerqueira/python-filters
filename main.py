1cb96f976dc8d9a2f8c53014eff66b4422844ff1
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2022-11-25 00:39:00 +0100

    update main + start story 8

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from filters import black_white, blur, dilation, gif_generator, img_from_video
-import os, sys, click, re
+from filters import black_white, blur, dilation
+from gif_generator import gif_generator
+from img_from_video import img_from_video
+import os, sys, click
 
 
 isDirectory = os.path.isdir("output")
@@ -16,10 +18,12 @@ dilation.dilation("imgs/megumin.jpeg",10,"output")
 @click.option('-i', default="imgs", help='--input-dir <directory>')
 @click.option('-o', default="output", help='--output-dir <directory>')
 @click.option('--filters', default="None", help="Choose filter from blur, gaussianblur, medianblur, dilate, grayscale")
+@click.option('--log-file', help="Log all actions in a file")
 @click.option('--output-format', help="Choose the name of the gif")
 @click.option('--video', help="Choose a video to transform into images")
 
-def main(i,filters,output_format,video,o):
+
+def main(i,filters,log_file,output_format,video,o):
     def apply_filter(input,filtr,output):
         list_filter = []
         args = filtr.split("|")
@@ -66,10 +70,10 @@ def main(i,filters,output_format,video,o):
         for name in list_of_files:
             apply_filter(name,filters,o)
     if output_format is not None:
-        gif_generator.gif_generator(o,output_format,o)
+        gif_generator(o,output_format,o)
 
     if video is not None:
-        img_from_video.img_from_video(video, o)
+        img_from_video(video, o)
 
 if __name__ == '__main__':
     try:
